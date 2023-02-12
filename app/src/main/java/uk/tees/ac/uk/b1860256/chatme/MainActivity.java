a25e9d7fcc0356948085cff538e466a50f0ef74e
Todimu Adegoke <adegoketeee@gmail.com>
2023-02-12 18:51:24 +0000

    Initial commit

---
+++ app/src/main/java/uk/tees/ac/uk/b1860256/chatme/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.tees.ac.uk.b1860256.chatme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
