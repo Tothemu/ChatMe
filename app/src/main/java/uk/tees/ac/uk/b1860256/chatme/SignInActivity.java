4c323cfddd17a37af3e3243a2c86975b03ea2262
Todimu Adegoke <adegoketeee@gmail.com>
2023-03-19 16:17:50 +0000

    Sign In page design completed. Google Sign in added. Facebook Sign in added. Phone Sign in added.

---
+++ app/src/main/java/uk/tees/ac/uk/b1860256/chatme/SignInActivity.java
@@ -0,0 +1,16 @@
+package uk.tees.ac.uk.b1860256.chatme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        getSupportActionBar().hide();
+    }
+}
