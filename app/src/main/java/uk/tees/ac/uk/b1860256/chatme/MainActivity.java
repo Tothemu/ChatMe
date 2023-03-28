3e644459edba88fa7686fb04fe3035a277a45887
Todimu Adegoke <adegoketeee@gmail.com>
2023-03-28 03:21:39 +0100

    Settings, Group Chat and Logout menu successfully added.

--- app/src/main/java/uk/tees/ac/uk/b1860256/chatme/MainActivity.java
+++ app/src/main/java/uk/tees/ac/uk/b1860256/chatme/MainActivity.java
@@ -3,12 +3,31 @@ package uk.tees.ac.uk.b1860256.chatme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import uk.tees.ac.uk.b1860256.chatme.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
